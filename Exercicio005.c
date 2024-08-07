3f90765d581ebfa44b103aed737d0eb440d122c8
GuilhermeHalter <halter.guilhermenunes@gmail.com>
2024-08-07 20:19:05 -0300

    Done Exercise 005

---
+++ Exercicio005.c
@@ -0,0 +1,17 @@
+/*
+5) Escrever um programa que incremente uma variável com valor mínimo 0 e valor máximo 100 e as mostre no vídeo.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int x;
+
+    for(x=0;x<=100;x++){
+        printf("%i\n",x);
+    }
+    
+return 0;
+}
